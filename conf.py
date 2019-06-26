71c14e270077816459d925953ee9c15b414dc711
Wout Slakhorst <wout.slakhorst@nedap.com>
2019-06-26 10:00:24 +0200

    remove source link
    Signed-off-by: Wout Slakhorst <wout.slakhorst@nedap.com>

--- conf.py
+++ conf.py
@@ -101,6 +101,11 @@ intersphinx_mapping = {
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes", ]
 
+html_context = {
+    "display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+    "last_updated": True,
+    "commit": False,
+}
 html_show_sourcelink = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
